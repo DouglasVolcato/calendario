b4a912128b34c1d04a6918640e9bc713bc185dfb
Douglas Volcato <douglasvolcato@gmail.com>
2022-10-19 19:49:27 -0300

    feat: add UpdateAreaUseCase

---
+++ server/src/services/area/updateArea.ts
@@ -0,0 +1,15 @@
+import { Area } from "../../database/protocols/data/area-interface";
+import { AreaRepositoryInterface } from "../../database/protocols/repositories/area-repository-interface";
+import { UpdateAreaUseCaseInterface } from "./protocols/area-services-interface";
+
+export class UpdateAreaUseCase implements UpdateAreaUseCaseInterface {
+  repository: AreaRepositoryInterface;
+
+  constructor(repository: AreaRepositoryInterface) {
+    this.repository = repository;
+  }
+
+  async execute(name: string, areaBody: Area): Promise<boolean> {
+    return await this.repository.updateArea(name, areaBody);
+  }
+}
