c59fdd1b1d97edbacb3f730d891761225a0a2b8a
Douglas Volcato <douglasvolcato@gmail.com>
2022-10-19 19:50:12 -0300

    feat: add DeleteTaskUseCase

---
+++ server/src/services/task/deleteTask.ts
@@ -0,0 +1,14 @@
+import { TaskRepositoryInterface } from "../../database/protocols/repositories/task-repository-interface";
+import { DeleteTaskUseCaseInterface } from "./protocols/task-services-interface";
+
+export class DeleteTaskUseCase implements DeleteTaskUseCaseInterface {
+  repository: TaskRepositoryInterface;
+
+  constructor(repository: TaskRepositoryInterface) {
+    this.repository = repository;
+  }
+
+  async execute(areaName: string, taskId: number): Promise<boolean> {
+    return await this.repository.deleteTask(areaName, taskId);
+  }
+}
