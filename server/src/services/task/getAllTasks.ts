abf657501f36d7ef58c168d89732de847b17300b
Douglas Volcato <douglasvolcato@gmail.com>
2022-10-19 19:50:52 -0300

    feat: add GetAllTasksUseCase

---
+++ server/src/services/task/getAllTasks.ts
@@ -0,0 +1,15 @@
+import { Task } from "../../database/protocols/data/task-interface";
+import { TaskRepositoryInterface } from "../../database/protocols/repositories/task-repository-interface";
+import { GetAllTasksUseCaseInterface } from "./protocols/task-services-interface";
+
+export class GetAllTasksUseCase implements GetAllTasksUseCaseInterface {
+  repository: TaskRepositoryInterface;
+
+  constructor(repository: TaskRepositoryInterface) {
+    this.repository = repository;
+  }
+
+  async execute(areaName: string): Promise<Task[] | undefined> {
+    return await this.repository.getAllTasks(areaName);
+  }
+}
