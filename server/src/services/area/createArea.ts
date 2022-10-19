1d08bafd3eea13a3b91dbb37765d56db30478419
Douglas Volcato <douglasvolcato@gmail.com>
2022-10-19 19:48:04 -0300

    feat: add CreateAreaUseCase

---
+++ server/src/services/area/createArea.ts
@@ -0,0 +1,15 @@
+import { Area } from "../../database/protocols/data/area-interface";
+import { AreaRepositoryInterface } from "../../database/protocols/repositories/area-repository-interface";
+import { CreateAreaUseCaseInterface } from "./protocols/area-services-interface";
+
+export class CreateAreaUseCase implements CreateAreaUseCaseInterface {
+  repository: AreaRepositoryInterface;
+
+  constructor(repository: AreaRepositoryInterface) {
+    this.repository = repository;
+  }
+
+  async execute(areaBody: Area): Promise<boolean> {
+    return await this.repository.createArea(areaBody);
+  }
+}
