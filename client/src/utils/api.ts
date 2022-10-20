0400159790b42384d9f717392f8eff40af20c23b
Douglas Volcato <douglasvolcato@gmail.com>
2022-10-20 16:05:29 -0300

    chore: add api helper functions

---
+++ client/src/utils/api.ts
@@ -0,0 +1,74 @@
+import { AreaInterface } from "../protocols/data/area-interface";
+
+const baseUrl = "http://localhost:3002";
+
+export const apiArea = {
+  getAllAreas: async (): Promise<{ foundAreas: AreaInterface[] }> => {
+    const response = await fetch(baseUrl + "/area/get-all-areas", {
+      method: "GET",
+      headers: new Headers({
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      }),
+    });
+    const data = await response.json();
+    return data;
+  },
+
+  deleteArea: async (areaName: string): Promise<boolean> => {
+    const response = await fetch(baseUrl + "/area/delete-area/" + areaName, {
+      method: "DELETE",
+      headers: new Headers({
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      }),
+    });
+    const data = await response.json();
+    return data;
+  },
+
+  editAreaName: async (
+    areaName: string,
+    newAreaName: string
+  ): Promise<boolean> => {
+    const response = await fetch(baseUrl + "/area/update-area/" + areaName, {
+      method: "PUT",
+      headers: new Headers({
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      }),
+      body: JSON.stringify({ name: newAreaName }),
+    });
+    const data = await response.json();
+    return data;
+  },
+
+  createArea: async (areaName: string): Promise<boolean> => {
+    const response = await fetch(baseUrl + "/area/create-area", {
+      method: "POST",
+      headers: new Headers({
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      }),
+      body: JSON.stringify({ name: areaName }),
+    });
+    const data = await response.json();
+    return data;
+  },
+};
+
+export const apiTask = {
+  getAllTasks: async (
+    areaName: string
+  ): Promise<{ foundAreas: AreaInterface[] }> => {
+    const response = await fetch(baseUrl + "/task/get-all-tasks/" + areaName, {
+      method: "GET",
+      headers: new Headers({
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      }),
+    });
+    const data = await response.json();
+    return data;
+  },
+};
