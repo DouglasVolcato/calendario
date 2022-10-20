7d38e80cd5a0f99cfcf20a21f11888c4d0281b60
Douglas Volcato <douglasvolcato@gmail.com>
2022-10-20 01:47:39 -0300

    feat: add some mock data

--- server/src/database/mocks/calendar.ts
+++ server/src/database/mocks/calendar.ts
@@ -1,3 +1,62 @@
 import { AreaInterface } from "../protocols/data/area-interface";
 
-export const calendar: AreaInterface[] = [];
+export const calendar: AreaInterface[] = [
+  {
+    name: "Configurações",
+    tasks: [
+      {
+        id: "u4m8-ffj6-4o6j-rh0v",
+        name: "Dar suporte",
+        description:
+          "A equipe de suporte precisa saber que a compilação final do programa complexificou o merge no fechamento automático das tags.",
+        deadline: "2022-10-20",
+        agreedDeadline: "2022-10-20",
+        urgency: "Pode esperar",
+        gravity: "Pouco grave",
+        tendency: "Não irá mudar",
+        legalDemand: true,
+      },
+      {
+        id: "u5f6t-fwere-4werwj-wer0v",
+        name: "Explica pro Product",
+        description:
+          "Explica pro Product Onwer que a normalização da data deletou todas as entradas dos parametros passados em funções privadas.",
+        deadline: "2022-11-30",
+        agreedDeadline: "2022-12-01",
+        urgency: "Piora em médio prazo",
+        gravity: "Grave",
+        tendency: "Merece atenção, curto prazo",
+        legalDemand: true,
+      },
+    ],
+  },
+  {
+    name: "Financeiro",
+    tasks: [
+      {
+        id: "u5f6t-fwere-4werwj-wer0v",
+        name: "Dar commit",
+        description:
+          "Com este commit, o módulo de recursão paralela otimizou a renderização de uma compilação com tempo acima da media.",
+        deadline: "2022-11-20",
+        agreedDeadline: "2022-12-05",
+        urgency: "Piora em médio prazo",
+        gravity: "Grave",
+        tendency: "Merece atenção, curto prazo",
+        legalDemand: true,
+      },
+      {
+        id: "u4m8-ffj6-4o6j-rh0v",
+        name: "Falar com cliente",
+        description:
+          "Fala pro cliente que a compilação final do programa facilitou a resolução de conflito na organização alfanumérico dos arrays multidimensionais",
+        deadline: "2022-10-20",
+        agreedDeadline: "2022-10-20",
+        urgency: "Pode esperar",
+        gravity: "Pouco grave",
+        tendency: "Não irá mudar",
+        legalDemand: true,
+      },
+    ],
+  },
+];
