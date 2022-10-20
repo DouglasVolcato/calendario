b524ab735d94846b50328a922c17a29851b0e121
Douglas Volcato <douglasvolcato@gmail.com>
2022-10-20 16:06:14 -0300

    feat: add interfaces to area and task

---
+++ client/src/protocols/data/area-interface.ts
@@ -0,0 +1,6 @@
+import { TaskInterface } from "./task-interface";
+
+export interface AreaInterface {
+  name: string;
+  tasks: TaskInterface[];
+}
---
+++ client/src/protocols/data/task-interface.ts
@@ -0,0 +1,12 @@
+export interface TaskInterface {
+    id: string;
+    name: string;
+    description: string;
+    deadline: string | Date;
+    agreedDeadline: string | Date;
+    urgency: string;
+    gravity: string;
+    tendency: string;
+    legalDemand: boolean;
+  }
+  
