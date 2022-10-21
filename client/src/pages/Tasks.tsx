f55560d32eb743101da97851c7b72c001047a8be
Douglas Volcato <douglasvolcato@gmail.com>
2022-10-20 22:04:34 -0300

    refacot: moved task components to a task page

--- client/src/pages/Tasks.tsx
+++ client/src/pages/Tasks.tsx
@@ -1,3 +1,18 @@
-export function Tasks(){
-    
+import { AreaInterface } from "../protocols/data/area-interface";
+import { TaskInterface } from "../protocols/data/task-interface";
+import { CreateTask } from "./CreateTask";
+import { SearchTasks } from "./SearchTasks";
+
+interface Props {
+  areas: AreaInterface[];
+  createTask(areaName: string, taskBody: TaskInterface): void
+}
+
+export function Tasks({ areas, createTask }: Props) {
+  return (
+    <div className="Task">
+      <SearchTasks areas={areas} />
+      <CreateTask areas={areas} createTask={createTask}/>
+    </div>
+  );
 }
