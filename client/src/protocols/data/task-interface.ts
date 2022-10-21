8e3f66ee74a0e9d4f6476ed24953da6ae2d9d26c
Douglas Volcato <douglasvolcato@gmail.com>
2022-10-21 04:25:59 -0300

    fix: add id to task interface

--- client/src/protocols/data/task-interface.ts
+++ client/src/protocols/data/task-interface.ts
@@ -1,11 +1,11 @@
 export interface TaskInterface {
-    name: string;
-    description: string;
-    deadline: string;
-    agreedDeadline: string;
-    urgency: string;
-    gravity: string;
-    tendency: string;
-    legalDemand: boolean;
-  }
-  
+  id?: string;
+  name: string;
+  description: string;
+  deadline: string;
+  agreedDeadline: string;
+  urgency: string;
+  gravity: string;
+  tendency: string;
+  legalDemand: boolean;
+}
