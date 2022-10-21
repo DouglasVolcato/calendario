b8f852138044290a466e806c58027c6005f6ddde
Douglas Volcato <douglasvolcato@gmail.com>
2022-10-21 01:10:38 -0300

    chore: add calendar

--- client/src/pages/Calendar.tsx
+++ client/src/pages/Calendar.tsx
@@ -1,5 +1,7 @@
 import { useSearchParams } from "react-router-dom";
 import { AreaInterface } from "../protocols/data/area-interface";
+import { TaskCards } from "./TaskCards";
+import "../styles/Calendar.css";
 
 interface Props {
   areas: AreaInterface[];
@@ -7,24 +9,44 @@ interface Props {
 
 export function Calendar({ areas }: Props) {
   const [searchParams] = useSearchParams();
+  const selectedAreaName = searchParams.get("area");
   const from = searchParams.get("from");
   const to = searchParams.get("to");
-  //   const foundArea = areas.find(
-  //     (area) => area.name === searchParams.get("area")
-  //   );
 
-  const areaTasks = areas.find(
-    (area) => area.name === searchParams.get("area")
-  )?.tasks;
-  const allTasks = [];
+  const selectedArea =
+    selectedAreaName !== "unset"
+      ? areas.filter((area) => area.name === selectedAreaName)
+      : areas;
 
-  const foundArea = searchParams.get("area") !== "unset" ? areaTasks : [];
+  const months = [
+    { name: "", number: 0 },
+    { name: "Janeiro", number: 1 },
+    { name: "Fevereito", number: 2 },
+    { name: "Março", number: 3 },
+    { name: "Abril", number: 4 },
+    { name: "Maio", number: 5 },
+    { name: "Junho", number: 6 },
+    { name: "Julho", number: 7 },
+    { name: "Agosto", number: 8 },
+    { name: "Setembro", number: 9 },
+    { name: "Outubro", number: 10 },
+    { name: "Novembro", number: 11 },
+    { name: "Dezembro", number: 12 },
+  ];
 
   return (
     <div className="Calendar">
-      <p>{searchParams.get("area")}</p>
-      {foundArea?.map((task) => (
-        <div>{task.name}</div>
+      {months.map((month) => (
+        <div className={`month-${month.number}`}>{month.name}</div>
+      ))}
+      {selectedArea.map((area) => (
+        <div className="Calendar-areas">
+          {months.map((item, index) => (
+            <div>
+              <TaskCards area={area} index={index} from={from} to={to} />
+            </div>
+          ))}
+        </div>
       ))}
     </div>
   );
