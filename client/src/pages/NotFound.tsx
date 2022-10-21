b0bd206fe0a5f395b48f9508eaa6f44eb0a2176e
Douglas Volcato <douglasvolcato@gmail.com>
2022-10-21 04:45:34 -0300

    feat: add not found page

---
+++ client/src/pages/NotFound.tsx
@@ -0,0 +1,10 @@
+import "../styles/NotFound.css";
+
+export default function NotFound() {
+  return (
+    <div className="NotFound">
+      <h1 className="NotFound-title">Erro 404</h1>
+      <p className="NotFound-description">Página não encontrada.</p>
+    </div>
+  );
+}
